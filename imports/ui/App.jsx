e187d2db636c31215286ce38fd4dc50b864aa32e
unkei <unkei@yahoo.co.jp>
2017-08-25 18:31:15 +0900

    added the filter for completed tasks by React state.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,14 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        }
+    }
+
     handleSubmit() {
         event.preventDefault();
 
@@ -24,8 +32,19 @@ class App extends Component {
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
+
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -35,6 +54,16 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
 
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                         <input
