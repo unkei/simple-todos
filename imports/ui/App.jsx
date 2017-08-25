c6190a113c2869c3efdd5a01617c0bc266a05092
unkei <unkei@yahoo.co.jp>
2017-08-25 14:57:20 +0900

    added a form to add new todo item.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks.js';
@@ -7,6 +8,21 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+    handleSubmit() {
+        event.preventDefault();
+
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
+
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
     renderTasks() {
         return this.props.tasks.map((task) => (
             <Task key={task._id} task={task} />
@@ -18,6 +34,14 @@ class App extends Component {
             <div className="container">
                 <header>
                     <h1>Todo List</h1>
+
+                    <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                        <input
+                            type="text"
+                            ref="textInput"
+                            placeholder="Type to add new tasks"
+                        />
+                    </form>
                 </header>
 
                 <ul>
