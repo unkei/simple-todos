34f5821495693b1ce1a21b812d45070a07664dda
unkei <unkei@yahoo.co.jp>
2017-08-25 23:34:49 +0900

    filtering data with publish and subscribe.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -7,7 +7,12 @@ export const Tasks = new Mongo.Collection('tasks');
 if (Meteor.isServer) {
     // This code only runs on the server
     Meteor.publish('tasks', function taskPublication() {
-        return Tasks.find();
+        return Tasks.find({
+            $or: [
+                { private: { $ne: true } },
+                { owner: this.userId },
+            ],
+        });
     });
 }
 
@@ -24,6 +29,7 @@ Meteor.methods({
             text,
             createdAt: new Date(),
             checked: false,
+            // private: false,
             owner: Meteor.userId(),
             username: Meteor.user().username,
         });
@@ -31,12 +37,37 @@ Meteor.methods({
     'tasks.remove'(taskId) {
         check(taskId, String);
 
+        const task = Tasks.findOne(taskId);
+        if (task.private && task.owner !== Meteor.userId()) {
+            // If the task is private, make sure only the onwer can delete it
+            throw new Meteor.Error('not-authorized');
+        }
+
         Tasks.remove(taskId);
     },
     'tasks.setChecked'(taskId, setChecked) {
         check(taskId, String);
         check(setChecked, Boolean);
 
+        const task = Tasks.findOne(taskId);
+        if (task.private && task.owner !== Meteor.userId()) {
+            // If the task is private, make sure only the onwer can check it off
+            throw new Meteor.Error('not-authorized');
+        }
+
         Tasks.update(taskId, { $set: { checked: setChecked } });
     },
+    'tasks.setPrivate'(taskId, setToPrivate) {
+        check(taskId, String);
+        check(setToPrivate, Boolean);
+
+        const task = Tasks.findOne(taskId);
+
+        // Make sure only the task owner can make a task private
+        if (task.owner !== Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.update(taskId, { $set: { private: setToPrivate } });
+    }
 });
