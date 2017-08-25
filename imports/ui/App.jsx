b8764d6febfb805c11271b696b28d4954562d50d
unkei <unkei@yahoo.co.jp>
2017-08-25 18:41:36 +0900

    added counter of incomplete tasks.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -53,7 +53,7 @@ class App extends Component {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
 
                     <label className="hide-completed">
                         <input
@@ -84,10 +84,12 @@ class App extends Component {
 
 App.PropTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 }
 
 export default createContainer(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    }
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
+    };
 }, App);
